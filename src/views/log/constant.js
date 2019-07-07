3f3c55e15d64a77deaafeb086b454125e3829514
nickle <993831927@qq.com>
2019-07-07 13:58:07 +0800

    fixed

--- src/views/log/constant.js
+++ src/views/log/constant.js
@@ -1,9 +1,6 @@
 import commonUtils from '@/utils/commonUtils'
 
-const statusList = [{ key: 0, value: '失败' }, { key: 1, value: '成功' }, { key: 2, value: '执行中' }, {
-  key: 3,
-  value: '等待执行'
-}]
+const statusList = [{ key: 0, value: '失败' }, { key: 1, value: '成功' }, { key: 2, value: '执行中' }]
 export default {
   statusMap: commonUtils.listToMap(statusList)
 }
